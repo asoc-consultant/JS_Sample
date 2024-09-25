a9ee9fda632eee6b3bbcc50b6ebe8495b9a96e17
asoc-consultant <154931629+asoc-consultant@users.noreply.github.com>
2024-09-25 17:41:29 -0400

    Add files via upload

---
+++ test-functions.js
@@ -0,0 +1,16 @@
+
+function aboutJSFunction() {
+var Loc01 = document.getElementById("Location01");
+Loc01.innerHTML="JavaScript is a simple language for coding";
+}
+
+
+function ssnNumFunction() {
+// using ssn numbers for test 267-04-5432 or 264574203
+var Loc05 = document.getElementById("Location05");
+Loc05.innerHTML="SSN Formatted: 267-04-5432 or  SSN Unformatted: 264574203";
+}
+
+
+
+
