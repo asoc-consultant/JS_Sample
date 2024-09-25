5b072b1d8260cd94247d32f01843e88720cc0cf2
asoc-consultant <154931629+asoc-consultant@users.noreply.github.com>
2024-09-25 18:18:59 -0400

    Update test-functions.js

--- test-functions.js
+++ test-functions.js
@@ -4,6 +4,11 @@ var Loc01 = document.getElementById("Location01");
 Loc01.innerHTML="JavaScript is a simple language for coding";
 }
 
+function ccNumFunction() {
+// using cc numbers for test CC Visa: 4012-8888-8888-1881  CC Diners: 38520000023237
+var Loc04 = document.getElementById("Location04");
+Loc04.innerHTML="CC Visa: 4012-8888-8888-1881  CC Diners: 38520000023237";
+}
 
 function ssnNumFunction() {
 // using ssn numbers for test 267-04-5432 or 264574203
